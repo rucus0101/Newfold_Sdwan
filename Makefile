b530e1110145c530497fb728c2456e341aadcacb
matt newsom <rucus01@gmail.com>
2024-08-16 12:53:02 +0000

    initial commit

---
+++ Makefile
@@ -0,0 +1,85 @@
+CURRENT_DIR = $(shell pwd)
+# option to run ansible-test sanity: must be either venv or docker (default is venv)
+ANSIBLE_TEST_MODE ?= venv
+MUFFET_TIMEOUT ?= 60
+
+.PHONY: help
+help: ## Display help message
+	@grep -E '^[0-9a-zA-Z_-]+\.*[0-9a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+#########################################
+# Ansible Collection actions            #
+#########################################
+.PHONY: collection-build
+collection-build: ## Build arista.avd collection locally.
+	./python-avd/scripts/build-schemas.py
+	ansible-galaxy collection build --force ansible_collections/arista/avd
+
+#########################################
+# pyavd actions                         #
+#########################################
+.PHONY: pyavd-build
+pyavd-build: ## Build PyAVD Python package locally.
+	cd python-avd && $(MAKE) clean build
+
+.PHONY: pyavd-test
+pyavd-test: ## Test PyAVD Python code with tox.
+	cd python-avd && $(MAKE) clean && tox -r
+
+.PHONY: pyavd-publish
+pyavd-publish: ## Build and publish PyAVD Python package.
+	cd python-avd && $(MAKE) clean build publish
+
+.PHONY: pyavd-install
+pyavd-install: pyavd-build ## Build and install PyAVD Python package.
+	pip install python-avd/dist/*
+
+# The editable_mode=compat is required for pylance to pick up the editable install.
+.PHONY: pyavd-editable-install
+pyavd-editable-install: ## Build and install PyAVD as editable
+	pip install -e python-avd --config-settings editable_mode=compat
+
+#########################################
+# Code Validation using ansible-test 	#
+#########################################
+
+.PHONY: sanity
+sanity: sanity-info sanity-lint sanity-import ## Run ansible-test sanity validation.
+
+.PHONY: sanity-info
+sanity-info: ## Show information about ansible-test.
+	cd ansible_collections/arista/avd/ ; ansible-test env
+
+.PHONY: sanity-lint
+sanity-lint: ## Run ansible-test sanity for code sanity. Specify `ANSIBLE_TEST_MODE=<venv|docker>` (default: `venv`).
+	cd ansible_collections/arista/avd/ ; \
+	ansible-test sanity --requirements --$(ANSIBLE_TEST_MODE) --skip-test import
+
+.PHONY: sanity-import
+sanity-import: ## Run ansible-test sanity for code import. Specify `ANSIBLE_TEST_MODE=<venv|docker>` (default: `venv`).
+	cd ansible_collections/arista/avd/ ; \
+	ansible-test sanity --requirements --$(ANSIBLE_TEST_MODE) --test import
+
+.PHONY: galaxy-importer
+galaxy-importer:  ## Run galaxy importer tests.
+	rm -f *.tar.gz && \
+	ansible-galaxy collection build --force ansible_collections/arista/avd && \
+	python -m galaxy_importer.main *.tar.gz
+
+#############################################
+# Run unit test cases using ansible-test    #
+#############################################
+
+.PHONY: unit-tests
+unit-tests: ## Run unit test cases using ansible-test. Specify `ANSIBLE_TEST_MODE=<venv|docker>` (default: `venv`).
+	cd ansible_collections/arista/avd/ ; \
+	ansible-test units --requirements --$(ANSIBLE_TEST_MODE) -vv
+
+###################################################
+# Run integration test cases using ansible-test   #
+###################################################
+
+.PHONY: integration-tests
+integration-tests: ## Run integration test cases using ansible-test. Specify `ANSIBLE_TEST_MODE=<venv|docker>` (default: `venv`).
+	cd ansible_collections/arista/avd/ ; \
+	ansible-test integration --requirements --$(ANSIBLE_TEST_MODE)
---
+++ python-avd/pyavd/avd_schema_tools.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2023-2024 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Generator
+
+from .validation_result import ValidationResult
+
+if TYPE_CHECKING:
+    from .vendor.errors import AvdDeprecationWarning
+
+
+class AvdSchemaTools:
+    """
+    Tools that wrap the various schema components for easy use
+    """
+
+    def __init__(self, schema: dict = None, schema_id: str = None) -> None:
+        """
+        Convert data according to the schema (convert_types)
+        The data conversion is done in-place (updating the original "data" dict).
+
+        Args:
+            schema_id:
+                Name of AVD Schema to use for conversion and validation.
+        """
+        from .vendor.schema.avdschema import AvdSchema  # pylint: disable=import-outside-toplevel
+
+        self.avdschema = AvdSchema(schema=schema, schema_id=schema_id)
+
+    def convert_data(self, data: dict) -> list[AvdDeprecationWarning]:
+        """
+        Convert data according to the schema (convert_types)
+        The data conversion is done in-place (updating the original "data" dict).
+
+        Args:
+            data:
+                Input variables which should be converted according to the schema.
+
+        Returns:
+            List of AvdDeprecationWarnings
+        """
+        from .vendor.errors import AvdConversionWarning, AvdDeprecationWarning  # pylint: disable=import-outside-toplevel
+
+        # avdschema.convert returns a Generator, so we have to iterate through it to perform the actual conversions.
+        exceptions: Generator = self.avdschema.convert(data)
+
+        result = []
+        for exception in exceptions:
+            # Ignore conversions
+            if isinstance(exception, AvdConversionWarning):
+                continue
+
+            # Store but continue for deprecations
+            if isinstance(exception, AvdDeprecationWarning):
+                result.append(exception)
+                continue
+
+            # Raise on other exceptions
+            if isinstance(exception, Exception):
+                raise exception
+
+        return result
+
+    def validate_data(self, data: dict) -> ValidationResult:
+        """
+        Validate data according to the schema
+
+        Args:
+            data:
+                Input variables which are to be validated according to the schema.
+
+        Returns:
+            Validation result object with any validation errors or deprecation warnings.
+        """
+        from .vendor.errors import AvdConversionWarning, AvdDeprecationWarning, AvdValidationError  # pylint: disable=import-outside-toplevel
+
+        result = ValidationResult(failed=False)
+
+        # avdschema.validate returns a Generator, so we have to iterate through it to perform the actual validations.
+        exceptions: Generator = self.avdschema.validate(data)
+        for exception in exceptions:
+            # Ignore conversions
+            if isinstance(exception, AvdConversionWarning):
+                continue
+
+            # Store and fail but continue for validation errors
+            if isinstance(exception, AvdValidationError):
+                result.validation_errors.append(exception)
+                result.failed = True
+                continue
+
+            # Store but continue for deprecations
+            if isinstance(exception, AvdDeprecationWarning):
+                result.deprecation_warnings.append(exception)
+                continue
+
+            # Raise on other exceptions
+            if isinstance(exception, Exception):
+                raise exception
+
+        return result
+
+    def convert_and_validate_data(self, data: dict) -> dict:
+        """
+        Convert and validate data according to the schema
+
+        Returns dictionary to be compatible with Ansible plugin. Called from vendored "get_structured_config".
+
+        Args:
+            data:
+                Input variables which are to be validated according to the schema.
+
+        Returns
+            dict :
+                failed : bool
+                    True if data is invalid. Otherwise False.
+                errors : list[Exception]
+                    Any data validation issues.
+        """
+        self.convert_data(data)
+        res = self.validate_data(data)
+        return {"failed": res.failed, "errors": res.validation_errors}
